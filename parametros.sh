4100595783ddf5653dd9593663b46dbb24509208
Gerardociscoman <57124182+Gerardociscoman@users.noreply.github.com>
2025-10-02 10:44:15 -0700

    Update parametros.sh

--- parametros.sh
+++ parametros.sh
@@ -5,7 +5,7 @@ VERSION=$2
 
 echo "Ejecutando deploy con variables..."
 echo "Ambiente: $ENVIRONMENT"
-echo "Versión: $APP_VERSION"
+echo "Versión: $VERSION"
 
 # Llamar al script del repo
 #bash scripts/deploy.sh "$ENVIRONMENT" "$APP_VERSION"
